dc67455535acb40e3751156843a3e5fda4adc26d
Siva <siva@fusioncharts.com>
2015-08-02 15:27:17 +0530

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsAggregatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newsAggregatorApp
+ */
+angular.module('newsAggregatorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsAggregatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newsAggregatorApp
+ */
+angular.module('newsAggregatorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
