703af46eeaedbd9467c95e1b1921b6a8f451e4b6
subramaniashiva <vengaishiva@gmail.com>
2015-08-06 22:57:51 +0530

    Added local storage feature

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,15 @@
  * Controller of the newsAggregatorApp
  */
 angular.module('newsAggregatorApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, localStorageService) {
     $scope.todos = [];
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
